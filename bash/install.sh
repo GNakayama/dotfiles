3ebe6c7163ceb87eac785c24ad1722e1e77892be
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2022-11-08 10:40:29 -0300

    Remove bash files before creating it

--- bash/install.sh
+++ bash/install.sh
@@ -4,6 +4,22 @@ if [ ! -d "$HOME/.extra_aliases" ]; then
 	/bin/touch "$HOME/.extra_aliases"
 fi
 
+if [ -f "$HOME/.aliases" ]; then
+	/bin/rm "$HOME/.aliases"
+fi
+
+if [ -f "$HOME/.bash_prompt" ]; then
+	/bin/rm "$HOME/.bash_prompt"
+fi
+
+if [ -f "$HOME/.bashrc" ]; then
+	/bin/rm "$HOME/.bashrc"
+fi
+
+if [ -f "$HOME/.env_vars" ]; then
+	/bin/rm "$HOME/.env_vars"
+fi
+
 /bin/ln .bash_prompt $HOME/.bash_prompt
 /bin/ln .bashrc $HOME/.bashrc
 /bin/ln .aliases $HOME/.aliases
