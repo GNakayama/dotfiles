1e244df3861982c2a4beeac4db6d84040396dd04
Guilherme <guilherme.nakayama.s@gmail.com>
2024-06-04 18:16:10 -0300

    Tokyo/Dracula theme

--- i3/install.sh
+++ i3/install.sh
@@ -2,3 +2,6 @@
 /bin/ln config $HOME/.config/autostart/display-setup.sh
 ## Install dejavu fonts
 ## Link XResource
+# Install TokyoNightTheme
+# Install Numix Icons
+#https://github.com/stronk-dev/Tokyo-Night-Linux
