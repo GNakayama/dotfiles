c9ca7b468e9cf858c89114b623a2044f5363a07b
Guilherme <guilherme.nakayama.s@gmail.com>
2024-06-04 21:05:28 -0300

    Initial lua migration

---
+++ vim/lua/configs/default.lua
@@ -0,0 +1,16 @@
+-- vim.o.nocompatible = true -- required
+-- vim.o.encoding = 'utf-8'  --The encoding displayed.
+-- vim.o.fileencoding = 'utf-8'  -- The encoding written to file.
+-- vim.o.hls = true
+-- vim.o.is = true
+-- vim.o.nrformats = true
+
+-- -- Enable folding
+-- vim.o.foldmethod = indent
+-- vim.o.foldlevel = 99
+
+-- vim.cmd("filetype plugin indent on") -- required
+-- vim.cmd("syntax enable")
+
+-- -- Color scheme
+-- vim.cmd("colorscheme tokyonight-storm")
