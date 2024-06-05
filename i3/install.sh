9ab1febc6cfa4d0f2010cb661ded69f4550fc115
Guilherme <guilherme.nakayama.s@gmail.com>
2024-06-05 17:13:36 -0300

    Remove i3 changes

--- i3/install.sh
+++ i3/install.sh
@@ -29,9 +29,3 @@ i3-msg restart
 
 
 
-/bin/ln config $HOME/.config/autostart/display-setup.sh
-## Install dejavu fonts
-## Link XResource
-# Install TokyoNightTheme
-# Install Numix Icons
-#https://github.com/stronk-dev/Tokyo-Night-Linux
