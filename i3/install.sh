27c960ced3ccfdfc57c53395f039d2cc10ce69b8
Guilherme <guilherme.nakayama.s@gmail.com>
2024-06-05 11:43:19 -0300

    Update install script

--- i3/install.sh
+++ i3/install.sh
@@ -1,7 +1,30 @@
+/bin/rm $HOME/.i3/config
 /bin/ln config $HOME/.i3/config
-/bin/ln config $HOME/.config/autostart/display-setup.sh
-## Install dejavu fonts
-## Link XResource
-# Install TokyoNightTheme
-# Install Numix Icons
-#https://github.com/stronk-dev/Tokyo-Night-Linux
+
+/bin/rm $HOME/.Xresources
+/bin/ln .Xresources $HOME/.Xresources
+xrdb $HOME/.Xresources
+
+sudo /bin/rm /etc/lightdm/slick-greeter.conf
+sudo /bin/mv slick-greeter.conf /etc/lightdm/slick-greeter.conf
+
+/bin/rm $HOME/.config/autostart/display-setup.sh
+/bin/ln display-setup.sh $HOME/.config/autostart/display-setup.sh
+
+# Background
+sudo /bin/mv backgrounds/bg.jpg /usr/share/backgrounds/bg.jpg
+sudo /bin/mv backgrounds/bg2.jpg /usr/share/backgrounds/bg2.jpg
+nitrogen
+
+# Theme
+git clone https://github.com/stronk-dev/Tokyo-Night-Linux
+sudo /bin/mv Tokyo-Night-Linux/usr/share/themes/TokyoNight /usr/share/themes/TokyoNight
+/bin/rm -rf Tokyo-Night-Linux
+
+yay -S numix-icon-theme-git
+
+lxappearance
+i3-msg restart
+
+
+
