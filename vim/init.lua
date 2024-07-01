b4c317ed2ab5296dacd5f837ae6fd63a39accfee
Guilherme <guilherme.nakayama.s@gmail.com>
2024-07-01 01:23:01 -0300

    Start vim to lua conversion

--- vim/init.lua
+++ vim/init.lua
@@ -1,15 +1,15 @@
-require("lua.configs.default")
-require('lua.config.maps')
+require("plugins.lazy")
+require("configs.default")
+require('configs.maps')
 
-require("lua.plugins.lazy")
 --require('plugins.lspconfig')
 require('lualine').setup()
 require('telescope').setup()
 require("CopilotChat").setup()
 
 require'nvim-web-devicons'.get_icons()
-require'lspconfig'.pyright.setup()
+--require'lspconfig'.pyright.setup()
 require("bufferline").setup()
-require('lualine').setup{ 
+require('lualine').setup{
   options = { theme  = 'moonfly', },
 }
