ec8499572b7e0cc4a3e55db07164976addf2aa2b
Guilherme <guilherme.nakayama.s@gmail.com>
2024-09-10 15:40:33 +0100

    Replace simple quotes with double quotes

--- vim/init.lua
+++ vim/init.lua
@@ -1,15 +1,13 @@
 require("plugins.lazy")
 require("configs.default")
-require('configs.maps')
+require("configs.maps")
 
---require('plugins.lspconfig')
-require('lualine').setup()
-require('telescope').setup()
+require("lualine").setup()
+require("telescope").setup()
 require("CopilotChat").setup()
 
-require'nvim-web-devicons'.get_icons()
---require'lspconfig'.pyright.setup()
+require("nvim-web-devicons").get_icons()
 require("bufferline").setup()
-require('lualine').setup{
-  options = { theme  = 'moonfly', },
+require("lualine").setup{
+  options = { theme  = "moonfly", },
 }
