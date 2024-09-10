20f083c6ddc17e24be3f9287b21e24e6f0ef2517
Guilherme <guilherme.nakayama.s@gmail.com>
2024-09-10 18:00:17 +0100

    Convert vim backup to lua

--- vim/lua/configs/backup.lua
+++ vim/lua/configs/backup.lua
@@ -1,14 +1,11 @@
----- Backups
---if has('persistent_undo')
---	set undodir=~/.vim/tmp/undo//     -- undo files
---	set undofile
---	set undolevels=3000
---	set undoreload=10000
---endif
+if vim.fn.has('persistent_undo') == 1 then
+    vim.opt.undodir = "~/.vim/tmp/undo//"
+    vim.opt.undofile = true
+    vim.opt.undolevels = 3000
+    vim.opt.undoreload = 10000
+end
 
---set backupdir=~/.vim/tmp/backup// -- backups
---set directory=~/.vim/tmp/swap//   -- swap files
---set backup
---set noswapfile
-
-
+vim.opt.backupdir = "~/.vim/tmp/backup//"
+vim.opt.directory = "~/.vim/tmp/swap//"
+vim.opt.backup = true
+vim.opt.swapfile = false
