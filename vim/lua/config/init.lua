a0b3b5d68c510d105b8063d8b8d75e188a7c219f
Guilherme <guilherme.nakayama.s@gmail.com>
2025-06-28 21:34:58 -0300

    Add diff tool

--- vim/lua/config/init.lua
+++ vim/lua/config/init.lua
@@ -77,4 +77,13 @@ require("codecompanion").setup({
       adapter = "gemini",
     },
   },
+  display = {
+    diff = {
+      enabled = true,
+      close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
+      layout = "vertical", -- vertical|horizontal split for default provider
+      opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
+      provider = "mini_diff"
+    },
+  },
 })
